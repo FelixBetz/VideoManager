5db529df1c964c4dffd846ae0aa44b92b97e2406
Felix Betz <betz.felix@web.de>
2025-02-20 11:23:06 +0100

    use runes

--- src/lib/VideoPlayer.svelte
+++ src/lib/VideoPlayer.svelte
@@ -1,82 +1,85 @@
 <script lang="ts">
-	export let src = '';
-	let videoElement: HTMLVideoElement;
-	let previewCanvas: HTMLCanvasElement;
-	let previewContext: CanvasRenderingContext2D | null = null;
+	let { src = '' } = $props();
+	let videoElement: HTMLVideoElement | undefined = $state();
+	let previewCanvas: HTMLCanvasElement | undefined = $state();
+	let previewContext: CanvasRenderingContext2D | null = $derived(
+		previewCanvas === undefined ? null : previewCanvas.getContext('2d')
+	);
 	let previewTime = 0;
-	let offscreenVideoElement: HTMLVideoElement;
+	let offscreenVideoElement: HTMLVideoElement | undefined = $state();
 
 	function handleMouseMove(event: MouseEvent) {
-		const rect = videoElement.getBoundingClientRect();
-		const x = event.clientX - rect.left;
-		const y = rect.bottom - previewCanvas.height - 30;
-		const percentage = x / rect.width;
-		previewTime = percentage * videoElement.duration;
-		previewCanvas.style.left = `${x}px`;
-		previewCanvas.style.top = `${y}px`; // Position above the cursor
+		if (videoElement && previewCanvas && offscreenVideoElement) {
+			const rect = videoElement.getBoundingClientRect();
+			const x = event.clientX - rect.left;
+			const y = rect.bottom - previewCanvas.height - 30;
+			const percentage = x / rect.width;
+			previewTime = percentage * videoElement.duration;
+			previewCanvas.style.left = `${x}px`;
+			previewCanvas.style.top = `${y}px`; // Position above the cursor
 
-		offscreenVideoElement.currentTime = previewTime;
-		offscreenVideoElement.pause();
-		offscreenVideoElement.onseeked = () => {
-			if (previewContext) {
-				previewContext.drawImage(
-					offscreenVideoElement,
-					0,
-					0,
-					previewCanvas.width,
-					previewCanvas.height
-				);
-			}
-		};
+			offscreenVideoElement.currentTime = previewTime;
+			offscreenVideoElement.pause();
+			offscreenVideoElement.onseeked = () => {
+				if (previewContext && previewCanvas) {
+					if (offscreenVideoElement) {
+						previewContext.drawImage(
+							offscreenVideoElement,
+							0,
+							0,
+							previewCanvas.width,
+							previewCanvas.height
+						);
+					}
+				}
+			};
+		}
 	}
 
 	function handleMouseLeave() {
-		previewCanvas.style.display = 'none';
+		if (previewCanvas) {
+			previewCanvas.style.display = 'none';
+		}
 	}
 
 	function handleMouseEnter() {
-		previewCanvas.style.display = 'block';
+		if (previewCanvas) {
+			previewCanvas.style.display = 'block';
+		}
 	}
 
 	function handleKeyDown(event: KeyboardEvent) {
 		if (event.key === 'f' || event.key === 'F') {
-			if (videoElement.requestFullscreen) {
+			if (videoElement && videoElement.requestFullscreen) {
 				videoElement.requestFullscreen();
 			}
 		}
 	}
-
-	$: if (previewCanvas) {
-		previewContext = previewCanvas.getContext('2d');
-	}
 </script>
 
-<h1>{src}</h1>
-{#if src != ''}
-	<div class="video-container">
-		<video
-			bind:this={videoElement}
-			width="640"
-			height="480"
-			controls
-			on:mousemove={handleMouseMove}
-			on:mouseleave={handleMouseLeave}
-			on:mouseenter={handleMouseEnter}
-			on:keydown={handleKeyDown}
-		>
-			<source {src} type="video/mp4" />
-			<track kind="captions" src="captions.vtt" srclang="en" label="English" />
-			Your browser does not support the video tag.
-		</video>
-		<div class="progress">
-			<canvas class="preview" bind:this={previewCanvas} width="160" height="90"></canvas>
-		</div>
-		<video bind:this={offscreenVideoElement} style="display: none;">
-			<source {src} type="video/mp4" />
-			<track kind="captions" src="captions.vtt" srclang="en" label="English" />
-		</video>
+<div class="video-container">
+	<video
+		bind:this={videoElement}
+		width="640"
+		height="480"
+		controls
+		onmousemove={handleMouseMove}
+		onmouseleave={handleMouseLeave}
+		onmouseenter={handleMouseEnter}
+		onkeydown={handleKeyDown}
+	>
+		<source {src} type="video/mp4" />
+		<track kind="captions" src="captions.vtt" srclang="en" label="English" />
+		Your browser does not support the video tag.
+	</video>
+	<div class="progress">
+		<canvas class="preview" bind:this={previewCanvas} width="160" height="90"></canvas>
 	</div>
-{/if}
+	<video bind:this={offscreenVideoElement} style="display: none;">
+		<source {src} type="video/mp4" />
+		<track kind="captions" src="captions.vtt" srclang="en" label="English" />
+	</video>
+</div>
 
 <style>
 	.preview {
