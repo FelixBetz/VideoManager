a5d06506dc785df73f4e0f32c07063d6e25efb75
Felix Betz <betz.felix@web.de>
2025-02-20 21:43:36 +0100

    higlight active menu item

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
+<nav class="bg-gray-800 p-4">
+	<ul class="flex space-x-4">
+		<li>
+			<a
+				href="/"
+				class="text-white no-underline hover:text-gray-400"
+				class:font-bold={$page.url.pathname === '/'}
+			>
+				Videos
+			</a>
+		</li>
+		<li>
+			<a
+				href="/add"
+				class="text-white no-underline hover:text-gray-400"
+				class:font-bold={$page.url.pathname === '/add'}
+			>
+				Add
+			</a>
+		</li>
+	</ul>
+</nav>
 {@render children()}
