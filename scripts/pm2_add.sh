e1a979afaf615acd587534ba647e0fcb8651ee26
febe2 <betz.felix@web.de>
2025-03-15 16:23:22 +0000

    add port and origion to pm2_add script

--- scripts/pm2_add.sh
+++ scripts/pm2_add.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-pm2 start npm --name video-manager -- run preview -- --host
-pm2 startup
-pm2 save
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+#set repo dir as working dir
+cd "$SCRIPT_DIR" 
+cd ..
+
+PORT=8765  ORIGIN=http://pivideo:8765   pm2 start node --name video-manager -- build
+
+
+pm2 startup
+pm2 save --force
 pm2 status
